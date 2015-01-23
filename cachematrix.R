7b9957d8f67ac8b86eecb023aa9548516fd48407
Arcapalo <Arcapalo@users.noreply.github.com>
2015-01-22 22:49:49 -0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function fix some objects in the memory
+## that can be reused.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    ## sets x equal to an empty matrix
+    ## Set the inverse equal to NULL
+    I <- NULL
+    set <- function(y){
+      ## set function assigns the argument to x
+      x <<- y
+      ## Once the set function is called, Inverse is re-set to NULL
+      ## (this is important if you redefine the matrix, x)
+      I <<- NULL
+    }
+    ## get function returns the matrix
+    get <- function() x
+    
+    ## setInverse overrides the previous value of I and assigns the argument to Inverse (which is
+    ## supposed to be the inverse of matrix x)
+    setInverse <- function(solve) I <<- solve
+    
+    ## getInverse returns the Inverse
+    getInverse <- function() I
+    
+    ## creates a list of the functions
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
-
+## This function returns the inverse matrix of the
+## provided in makeCacheMatrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getInverse()
+  if(!is.null(inverse)){
+    message("Returning INVERSE already in cache")
+    return(inverse)
+  }
+  message("Calculating INVERSE . . .")
+  data <- x$get()
+  ##I <- solve(data, ...)
+  inverse <- tryCatch(solve(data, ...),error=function(e) {print(paste("M is singular, cannot be inverted."))})
+  x$setInverse(inverse)
+  inverse
 }
